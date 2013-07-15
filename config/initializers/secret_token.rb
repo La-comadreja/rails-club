1c24e56087998f5354ce5cf7f8e3a8fae2c37bad
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-07-14 22:32:36 -0400

    Initialize Git repository from http://techazzist.wordpress.com/2012/01/01/creating-a-simple-ruby-on-rails-application-using-devise/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsClub::Application.config.secret_token = 'cf5b5f9f378a5885603d7532718fa99e06ed5ccdbf9f0770b2093fe3111ba4161800f783a4f15e6ac5cd86b5c933a6bde8703793f0bf1d35b7066ea6f1f46f27'
