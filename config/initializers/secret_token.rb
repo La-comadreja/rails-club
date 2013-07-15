768de6c5d87e4aeeb39edb8d7b799437e5f44bbc
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-07-14 23:15:46 -0400

    Recreate empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsClub::Application.config.secret_token = 'e34187825ab0192ad593c4931b82d6a81bf7964c6e80de3ee83238a5627761e8fbecb2b277a08ac06b8a87087c8d4dd26c4fb0bbb3e98fa4185e8a18e7d4315f'
